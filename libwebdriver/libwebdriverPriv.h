cbf2f550d97ef6af1de7cb85b32a4f40ca5ef81e
Eric Millin <eric@saucelabs.com>
2015-01-02 14:50:28 -0500

    Initial Commit

---
+++ libwebdriver/libwebdriverPriv.h
@@ -0,0 +1,19 @@
+/*
+ *  libwebdriverPriv.h
+ *  libwebdriver
+ *
+ *  Created by Sauce Labs on 1/2/15.
+ *  Copyright (c) 2015 Ox1mason. All rights reserved.
+ *
+ */
+
+/* The classes below are not exported */
+#pragma GCC visibility push(hidden)
+
+class libwebdriverPriv
+{
+	public:
+		void HelloWorldPriv(const char *);
+};
+
+#pragma GCC visibility pop
